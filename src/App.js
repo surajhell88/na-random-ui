45a6a5584e074009a4f556821685c9be21deb157
surajhell88 <surajhell88@gmail.com>
2020-12-16 21:16:03 +0530

    Adding font related changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
+import "./App.css";
 import close from "./close.svg";
 import tick from "./tick.svg";
 import refresh from "./reload.svg";
 import axios from "axios";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const peopleService = axios.create({
-  baseURL: 'https://na-random-service.herokuapp.com/'
+  baseURL: "https://na-random-service.herokuapp.com/",
 });
 
 function shuffle(array) {
-  let currentIndex = array.length, temporaryValue, randomIndex;
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
@@ -35,7 +36,9 @@ function App() {
   const createPerson = (e) => {
     e.preventDefault();
     setPerson("");
-    const existingPerson = people.find(p => p.name.toLowerCase() === person.toLowerCase());
+    const existingPerson = people.find(
+      (p) => p.name.toLowerCase() === person.toLowerCase()
+    );
     if (existingPerson) return;
     peopleService.post("/people", { name: person }).then(({ data }) => {
       setPeople([...people, data]);
@@ -43,9 +46,9 @@ function App() {
   };
   const removePerson = (id) => {
     peopleService.delete(`/people/${id}`).then(() => {
-      setPeople(people.filter(person => person.id !== id));
+      setPeople(people.filter((person) => person.id !== id));
     });
-  }
+  };
   const onPersonChange = (e) => {
     setPerson(e.target.value);
   };
@@ -53,9 +56,11 @@ function App() {
     setRandomisedPeople(shuffle([...people]));
   };
   const removeRandomisedPerson = (id) => {
-    setRandomisedPeople(randomisedPeople.filter(rPerson => rPerson.id !== id));
-  }
-  useEffect(randomisePeople, [people])
+    setRandomisedPeople(
+      randomisedPeople.filter((rPerson) => rPerson.id !== id)
+    );
+  };
+  useEffect(randomisePeople, [people]);
   useEffect(() => {
     peopleService.get("/people").then(({ data }) => {
       setPeople(data);
@@ -67,23 +72,29 @@ function App() {
       <div className="App">
         <div className="App-people">
           <form onSubmit={createPerson}>
-            <input value={person} onChange={onPersonChange} placeholder="Enter a name" />
+            <input
+              value={person}
+              onChange={onPersonChange}
+              placeholder="Enter a name"
+            />
           </form>
           <ul className="App-list">
-            {
-              people.map(person => {
-                return <li key={person.id}>
+            {people.map((person) => {
+              return (
+                <li key={person.id}>
                   <span>{person.name}</span>
-                  <span onClick={() => removePerson(person.id)} className="App-delete">
+                  <span
+                    onClick={() => removePerson(person.id)}
+                    className="App-delete"
+                  >
                     <img src={close} alt="delete" />
                   </span>
-                </li>;
-              })
-            }
+                </li>
+              );
+            })}
           </ul>
         </div>
-        {
-          people.length > 2 ?
+        {people.length > 2 ? (
           <div className="App-random">
             <div className="App-random-title">
               <h2>Randomised List</h2>
@@ -92,22 +103,24 @@ function App() {
               </span>
             </div>
             <ul className="App-list App-random-list">
-              {
-                randomisedPeople.map((person, i) => {
-                  return <li key={person.id}>
-                    <span>{person.name}</span>
-                    {
-                      i === 0 ?
-                      <span onClick={() => removeRandomisedPerson(person.id)} className="App-delete">
+              {randomisedPeople.map((person, i) => {
+                return (
+                  <li key={person.id}>
+                    <span className="App-random-item">{person.name}</span>
+                    {i === 0 ? (
+                      <span
+                        onClick={() => removeRandomisedPerson(person.id)}
+                        className="App-delete App-random-delete"
+                      >
                         <img src={tick} alt="delete" />
-                      </span> : null
-                    }
-                  </li>;
-                })
-              }
+                      </span>
+                    ) : null}
+                  </li>
+                );
+              })}
             </ul>
-          </div> : null
-        }
+          </div>
+        ) : null}
       </div>
       <h3 className="App-copyright">&#169; DataGuard</h3>
     </div>
